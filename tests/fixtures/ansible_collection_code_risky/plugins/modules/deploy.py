7ccdd2be503cdd6dcdaba32d5d70a8ca66ae6739
texasich <texasich@users.noreply.github.com>
2026-07-13 11:52:19 -0500

    test: use Ansible URL helper in risky fixture

--- tests/fixtures/ansible_collection_code_risky/plugins/modules/deploy.py
+++ tests/fixtures/ansible_collection_code_risky/plugins/modules/deploy.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
 
-import requests
 import yaml
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import open_url
 
 module = AnsibleModule(
     argument_spec=dict(api_token=dict(type="str", no_log=False)),
@@ -11,7 +11,7 @@ module = AnsibleModule(
 )
 api_token = os.environ.get("RTP_FIXTURE_ANSIBLE_SECRET_DO_NOT_LEAK")
 subprocess.run(module.params["command"], shell=True)
-requests.get(module.params["url"], verify=False)
+open_url(module.params["url"], validate_certs=False)
 os.remove(module.params["path"])
 yaml.load(module.params["document"])
 module.exit_json(changed=True)
